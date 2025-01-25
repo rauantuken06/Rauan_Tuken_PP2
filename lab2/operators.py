2c27cb661784103463bd5eb37c62ac4a3f657abe
rauantuken06 <r_tuken@kbtu.kz>
2025-01-25 16:26:56 +0600

    lab2

---
+++ lab2/operators.py
@@ -0,0 +1,40 @@
+print(10 + 5)
+
+x = 5
+print(x)
+
+x += 2
+print(x)
+
+x -= 4
+print(x)
+
+x *= 3
+print(x)
+
+x /= 2
+print(x)
+
+x %= 3
+print(x)
+
+y = 6
+if x == y:
+  print("x and y are equal")
+elif x > y:
+  print("x is greater than y")
+elif x < y:
+    print("x is less than y")
+else:
+    print("No comparison")
+
+print(x ** y)
+print(x // y)
+
+
+print((6 + 3) - (6 + 3))
+
+print(100 + 5 * 3)
+
+
+print(5 + 4 - 7 + 3)
